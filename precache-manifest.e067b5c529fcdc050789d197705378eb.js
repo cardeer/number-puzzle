a28a447ba7a0cf6454e53320992a1bb30d83743a
CARDEER <cardeer123_1@hotmail.com>
2020-10-12 13:07:25 +0700

    deploy

---
+++ precache-manifest.e067b5c529fcdc050789d197705378eb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8247c34f4a03c781f77b",
+    "url": "/number-puzzle/css/app.3a7bbe9f.css"
+  },
+  {
+    "revision": "afc3bdf9ad9a6e2a4b00",
+    "url": "/number-puzzle/css/chunk-886378ec.bf735faf.css"
+  },
+  {
+    "revision": "a9ff1405be805f5d27b2ac89b9f84cd0",
+    "url": "/number-puzzle/index.html"
+  },
+  {
+    "revision": "8247c34f4a03c781f77b",
+    "url": "/number-puzzle/js/app.74bbfb74.js"
+  },
+  {
+    "revision": "afc3bdf9ad9a6e2a4b00",
+    "url": "/number-puzzle/js/chunk-886378ec.1bcab4f5.js"
+  },
+  {
+    "revision": "e015325d0aed809938b6",
+    "url": "/number-puzzle/js/chunk-vendors.263c6e3f.js"
+  },
+  {
+    "revision": "95eb4ad1271de21c0303c185ca5b5688",
+    "url": "/number-puzzle/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/number-puzzle/robots.txt"
+  }
+]);
